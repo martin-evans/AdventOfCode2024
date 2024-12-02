7c7f123408e2672940c26f6d612af365e2c90ed2
Martin Evans <martin.evans@agiliosoftware.com>
2024-12-02 12:56:33 +0000

    PartOne Complete

--- AdventOfCode.Tests/DayTwo/AnalyserReport.cs
+++ AdventOfCode.Tests/DayTwo/AnalyserReport.cs
@@ -1,63 +1,39 @@
-using System.Runtime.CompilerServices;
-
 namespace AdventOfCode.Tests.DayTwo;
 
-public class AnalyserReport
+public static class AnalyserReport
 {
     public static int Calculate(string data)
     {
-        var result = 0;
-        
-        foreach (var row in data.Split("\n"))
-        {
-            
-            var rowData = row.Split(" ").Select(int.Parse).ToArray();
-
-            if (IsSameDirection(rowData) && IncrementsAreSafe(rowData))
-            {
-                result++;
-            }
-            
-        }
+        var ret = data.Split("\n")
+            .Select(row => row.Split(" ").Select(int.Parse).ToArray())
+            .Where(rowData => IsSameDirection(rowData) && IncrementsAreSafe(rowData));
 
 
-        return result;
+        return ret.Count();
     }
 
     private static bool IncrementsAreSafe(int[] data)
     {
-        
         var lst = new List<int>();
 
-        for (int i = 0; i < data.Length-2; i++)
+        for (var i = 0; i < data.Length - 1; i++)
         {
             lst.Add(data[i] - data[i + 1]);
         }
-        
-        //var diffs = data.Select((t, i) => data[i + 1] - t).ToList();
-        
-        return lst.ToArray().All(diff => diff is <= 2 and >= -2);
-        
+
+        return lst.TrueForAll(diff => diff is <= 3 and >= -3 and not 0);
     }
 
 
     private static bool IsSameDirection(int[] data)
     {
-
         var lst = new List<int>();
 
-        for (int i = 0; i < data.Length-2; i++)
+        for (var i = 0; i < data.Length - 1; i++)
         {
             lst.Add(data[i] - data[i + 1]);
         }
 
-
-        var diffs = lst.ToArray(); //data.Select((t, i) => data[i + 1] - t).ToList();
-        
-
-        
-        return diffs.All(diff => diff > 0) || diffs.All(diff => diff < 0);
-        
+        return lst.TrueForAll(d => d > 0) || lst.TrueForAll(d => d < 0);
     }
-    
 }
