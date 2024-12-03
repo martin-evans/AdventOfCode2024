4968a110e52e2b610c593b313c9326d85a0cb22f
Martin Evans <martin.evans@agiliosoftware.com>
2024-12-03 09:35:05 +0000

    Scenario2 complete

--- AdventOfCode.Tests/DayThree/UnCorrupter.cs
+++ AdventOfCode.Tests/DayThree/UnCorrupter.cs
@@ -27,4 +27,46 @@ public static class UnCorrupter
         return res;
         
     }
+
+    public static int Calculate2(string input)
+    {
+        var pattern = new Regex(@"mul\((\d+),(\d+)\)|don\'t\(\)|do\(\)");
+        var delimiters = new[] { "mul(", ",", ")" };
+
+        var matches = pattern.Matches(input);
+        
+        var res = 0;
+
+        var filteredMatches = new List<string>();
+
+        var include = true;
+        
+        foreach (Match match in matches)
+        {
+            include = match.Value switch
+            {
+                "don't()" => false,
+                "do()" => true,
+                _ => include
+            };
+
+            if (include && match.Value != "do()")
+            {
+                filteredMatches.Add(match.Value);
+            }
+            
+        }
+
+        filteredMatches.ForEach(x =>
+        {
+            var digits = x.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            res += digits[0] * digits[1];
+        });
+            
+
+        return res;
+    }
 }
